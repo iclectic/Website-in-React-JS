81e58140cec19fc336f52d33e720263be6a2badf
iclectic <tansonestost@yahoo.com>
2021-04-25 20:53:58 +0000

    From New Branch

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import CardItem from './CardItem';
 import './Cards.css';
+import image9 from "../images/img-9.jpg";
+import image2 from "../images/img-2.jpg";
+
 
 
 
@@ -12,12 +15,40 @@ function Cards() {
             <div className='cards__wrapper'>
                 <ul className='cards__items'>
                     <CardItem 
-                    src="/images/img-9.jpg"
+                    src={`${image9}`}
+
                     text="Explore the hidden watefall deep inside the amazon jungle"
                     label="Adventure"
                     path="/services"
+                    />  
+                   
+                    <CardItem 
+                    src={`${image2}`}
+
+                    text="Travel through this Island in Aja city"
+                    label="Aja"
+                    path="/services"
                     />
                 </ul>
+
+                <ul className='cards__items'>
+                    <CardItem 
+                    src={`${image9}`}
+
+                    text="Explore the hidden watefall deep inside the amazon jungle"
+                    label="Adventure"
+                    path="/services"
+                    />  
+                   
+                    <CardItem 
+                    src={`${image2}`}
+
+                    text="Travel through this Island in Aja city"
+                    label="Aja"
+                    path="/services"
+                    />
+                </ul> 
+
             </div>
             </div>
             </div>
